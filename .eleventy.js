3815fe1bab7bb10ca47726b9a8f402e9885cab00
Dmitry Iv <df.creative@gmail.com>
2024-10-07 23:22:06 -0400

    Add pathPrefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,18 @@
-module.exports = function (eleventyConfig) {
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+
+export default function (eleventyConfig) {
+  // configure base url
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
   // Copy the admin folder so that config.yml is available
   eleventyConfig.addPassthroughCopy("admin");
 
   // copy assets
   eleventyConfig.addPassthroughCopy("assets");
 
+
   return {
+    pathPrefix: "/festivaldescouleurs/",
     dir: {
       input: ".",
       includes: "_includes",
