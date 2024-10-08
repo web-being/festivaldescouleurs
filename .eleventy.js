67fc9a0e8fbb6c614a84d2b19d9d53cb69d95030
Dmitry Iv <df.creative@gmail.com>
2024-10-07 22:37:03 -0400

    Add static pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
+  // Copy the admin folder so that config.yml is available
+  eleventyConfig.addPassthroughCopy("admin");
+
   return {
     dir: {
       input: ".",
